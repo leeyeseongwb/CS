78bc52759ffb3f850bdfcb6d6497d4a88fbe535f
YESEONG LEE <leeyeseong@outlook.com>
2025-11-26 18:21:19 +0800

    10007번 풀기 완료

--- WBFoundation.py
+++ WBFoundation.py
@@ -1,12 +1,13 @@
 """import random
 
-# 1부터 10까지의 숫자 중 3개를 중복 없이 뽑기
-result = random.sample(range(16, 35), 18)
+print ("남자:")
+result = random.sample(range(1, 16), 15)
 print(result)
 
-result = random.sample(range(1, 16), 15)
-print(result)"""
-
+print ("여자:")
+result = random.sample(range(16, 34), 18)
+print(result)
+"""
 
 l = list(map(int, input().split()))
 
